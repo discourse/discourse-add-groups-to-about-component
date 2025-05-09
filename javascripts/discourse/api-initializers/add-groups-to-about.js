0259dc842b86b154b13feb8eaab60fed18a9b0d3
Ted Johansson <ted@discourse.org>
2025-05-09 09:42:19 +0800

    DEV: Add a flag to facilitate merging into core

--- javascripts/discourse/api-initializers/add-groups-to-about.js
+++ javascripts/discourse/api-initializers/add-groups-to-about.js
@@ -2,5 +2,12 @@ import { apiInitializer } from "discourse/lib/api";
 import AdditionalAboutGroups from "../components/additional-about-groups";
 
 export default apiInitializer("1.14.0", (api) => {
+  const siteSettings = api.container.lookup("service:site-settings");
+
+  // Functionality is moving into core.
+  if (siteSettings.show_additional_about_groups === true) {
+    return;
+  }
+
   api.renderInOutlet("about-after-moderators", AdditionalAboutGroups);
 });
